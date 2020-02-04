5afc1d04008bfb75b8f094d2d840ebb0d51c53ec
root <root@zld05458.vci.att.com>
2020-02-04 13:32:23 -0800

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export http_proxy=one.proxy.att.com:8080
+export https_proxy=one.proxy.att.com:8080
 chdir /home/wj3525/docker-test/django-test
 sudo /usr/local/bin/docker-compose up -d
