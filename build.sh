f33617fd7234f6f9682da7b3794839f2bedaee8e
root <root@zld05458.vci.att.com>
2020-02-04 13:38:59 -0800

    build.sh error fixing testing.

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 
 export http_proxy=one.proxy.att.com:8080
 export https_proxy=one.proxy.att.com:8080
-chdir /home/wj3525/docker-test/django-test
-sudo /usr/local/bin/docker-compose up -d
+sudo cd /home/wj3525/docker-test/django-test;/usr/local/bin/docker-compose up -d
