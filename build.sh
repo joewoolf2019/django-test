db91dac006ebe3168e60a9a07770ee328d790fb2
Joe Woolf <wj3525@zld05458.vci.att.com>
2020-02-04 15:05:38 -0800

    fixed build.sh again

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 export http_proxy=one.proxy.att.com:8080
 export https_proxy=one.proxy.att.com:8080
-sudo cd /home/wj3525/docker-test/django-test;/usr/local/bin/docker-compose build 
-sudo cd /home/wj3525/docker-test/django-test;/usr/local/bin/docker-compose up -d 
+sudo cd /home/wj3525/docker-test/django-test;sudo /usr/local/bin/docker-compose build 
+sudo cd /home/wj3525/docker-test/django-test;sudo /usr/local/bin/docker-compose up -d 
